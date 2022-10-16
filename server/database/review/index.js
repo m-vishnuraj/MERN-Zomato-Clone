2cdd4ec745b020a76ad5b748036687755628ca0d
m-vishnuraj <mvishnurajm@gmail.com>
2022-10-16 21:00:22 +0530

    Review Api is completed

--- server/database/review/index.js
+++ server/database/review/index.js
@@ -2,32 +2,22 @@ import mongoose from "mongoose";
 
 const ReviewSchema = new mongoose.Schema(
   {
-    food: {
-      type: mongoose.Types.ObjectId,
-      ref: "foods",
-    },
-    restaurant: {
-      type: mongoose.Types.ObjectId,
-      ref: "restaurants",
-    },
-    user: {
-      type: mongoose.Types.ObjectId,
-      ref: "users",
-    },
+    food: { type: mongoose.Types.ObjectId, ref: "foods" },
+    restaurant: { type: mongoose.Types.ObjectId, ref: "restaurants" },
+    user: { type: mongoose.Types.ObjectId, ref: "users" },
     rating: { type: Number, required: true },
     reviewText: { type: String, required: true },
     isRestaurantReview: Boolean,
     isFoodReview: Boolean,
-    photos: [
+    photos: 
       {
         type: mongoose.Types.ObjectId,
         ref: "images",
       },
-    ],
   },
   {
     timestamps: true,
   }
 );
 
-const ReviewModel = mongoose.model("reviews", ReviewSchema);
+export const ReviewModel = mongoose.model("reviews", ReviewSchema);
